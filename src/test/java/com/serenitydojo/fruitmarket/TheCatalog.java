aab81434fe02135502654861e64a6ba2fa15ab63
manuel76046 <kenseith92@hotmail.com>
2023-03-21 11:32:47 -0500

    test case 3 solved

--- src/test/java/com/serenitydojo/fruitmarket/TheCatalog.java
+++ src/test/java/com/serenitydojo/fruitmarket/TheCatalog.java
@@ -23,6 +23,8 @@ public class TheCatalog {
 
     @Test
     public void shouldBeAbleToUpdateTheCurrentPriceOfAFruit() {
+       //You can update the catalog with the current market price of a fruit
+
         //GIVEN
         Catalog catalog = new Catalog(fruitCatalog);
         //WHEN
@@ -33,6 +35,8 @@ public class TheCatalog {
 
     @Test
     public void shouldListNamesOfAvailableFruitInAlphabeticalOrder(){
+        //The Catalog should list the names of the currently available fruit in alphabetical order
+
         //GIVEN
         Catalog catalog = new Catalog(fruitCatalog);
         Map<String,Double> sortedFruits = new TreeMap<>(fruitCatalog);
@@ -40,10 +44,34 @@ public class TheCatalog {
         catalog.getFruitList(sortedFruits);
         //THEN
         assertThat(sortedFruits).containsOnlyKeys("Apple","Banana","Orange","Pear");
+    }
 
+   @Test
+    public void shouldValidateThePriceOfAFruit(){
+        //The Catalog should report the price of a given type of fruit
 
+       //GIVEN
+       Catalog catalog = new Catalog(fruitCatalog);
+       //WHEN
+       catalog.getPriceOf("Apple");
+       //THEN
+       assertThat(catalog.getPriceOf("Apple")).isEqualTo(1.00);
+    }
 
+    @Test
+    public void shouldThrowCustomExceptionWhenFruitIsNotAvailable(){
+        //The Catalog should throw a FruitUnavailableException if the fruit is not currently available
+
+        //GIVEN
+        Catalog catalog = new Catalog(fruitCatalog);
+        //WHEN
+
+        //THEN
 
     }
+
+
+
+
 
 }
